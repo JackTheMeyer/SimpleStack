11f5a3ca35449c0b70584b1f69dc0c8dd9bbf9ea
Jack <jackmeyer@live.com.au>
2023-05-03 20:11:47 +1000

    Basic console app for the SimpleStack. Has pop, push, and top functionality with a basic console text loop.

---
+++ SimpleStack/MySimpleStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStack
+{
+    class MySimpleStack
+    {
+        private List<int> _stack;
+        public MySimpleStack()
+        {
+            _stack = new List<int>();
+        }
+
+        public int Top => _stack.Last();
+
+        public int Count => _stack.Count;
+
+        public void Push(int value)
+        {
+            _stack.Add(value);
+        }
+
+        public void Pop()
+        {
+            _stack.Remove(_stack.Last());
+        }
+
+    }
+}
--- SimpleStack/Program.cs
+++ SimpleStack/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using SimpleStack;
+
+var stack = new MySimpleStack();
+string? answer = null;
+string commands = @"Commands:
+    top - view the top value of the stack\n
+    pop - pop off the top value of the stack\n
+    type an integer to push it to the stack!
+    exit to quit
+";
+
+while (answer != "exit")
+{
+    Console.WriteLine(commands);
+    answer = Console.ReadLine();
+    Console.WriteLine();
+    bool push = int.TryParse(answer, out int result);
+    if (push)
+    {
+        stack.Push(int.Parse(answer));
+        Console.WriteLine($"{answer} pushed onto stack.\n");
+    }
+    else
+    {
+        switch (answer)
+        {
+            case "top":
+                if (stack.Count > 0)
+                    Console.WriteLine($"Top value is: {stack.Top}\n");
+                else
+                    Console.WriteLine("Stack is empty!\n");
+                break;
+            case "pop":
+                if (stack.Count > 0)
+                {
+                    Console.WriteLine($"{stack.Top} popped off the stack.\n");
+                    stack.Pop();
+                }
+                else
+                    Console.WriteLine("Stack is empty!\n");
+                break;
+            default:
+                Console.WriteLine("Unrecognised input!\n");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
